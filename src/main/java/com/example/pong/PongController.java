da7bccd5f8c60b26235520a5cac7b0de59316d0a
isalvaus <salvadorgarcia@escuelaces.net>
2023-01-27 00:20:37 +0100

    Pane with paddles

---
+++ src/main/java/com/example/pong/PongController.java
@@ -0,0 +1,14 @@
+package com.example.pong;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+public class PongController {
+    public Pane Canvas;
+    public Rectangle LPaddle;
+    public Rectangle RPaddle;
+
+
+}
